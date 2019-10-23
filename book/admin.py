45a4e9d67cb4903aa81d9a7cd1acc85180fb3b25
Alireza <golrokh10@outlook.com>
2019-10-23 13:22:28 +0330

    create TabularInline for AuthorAdmin and BookAdmin, and set fieldsets for BookInstanceAdmin in Admin.py

--- book/admin.py
+++ book/admin.py
@@ -7,18 +7,33 @@ from .models import BookInstance
 
 # Register your models here.
 
+class AuthorAdminInline(admin.TabularInline):
+    model = Book
+
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('f_name', 'l_name', 'birth_date')
-    search_fields = ['l_name', 'f_name']    
-    fields = ('birth_date',)
+    search_fields = ['l_name', 'f_name']
+
+    inlines = [AuthorAdminInline]
+
+    fields = [
+                'f_name', 
+                'l_name', 
+                ('birth_date', 'death_date')
+            ]
     list_display_links = ('f_name', 'l_name')
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
     search_fields = ('title',)
+    inlines = [BookInstanceInline]
 
     def display_genre(self, obj):
         genres = [genre.name for genre in obj.genre.all()[:2]]
@@ -39,6 +54,16 @@ class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('due_back', 'status')
     search_fields = ('id', 'due_back')
 
+    fieldsets = (
+        (None, {
+            'fields':(
+                'description',
+                )
+        }),
+        ('availablility', {'fields':('due_back', 'status')})
+    )
+
     def book_title_display(self,obj):
         return obj.book.title
+
     book_title_display.short_description = 'title'
